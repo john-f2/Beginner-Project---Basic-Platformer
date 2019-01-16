69e522493d9eb0fb84e230a67bb4b9069ee2d30c
John Fu <fujohn2@hotmail.com>
2019-01-15 17:19:02 -0800

    Create Score and health Scripts
    Also added in the sprites for coin and chest

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * 
+ * Player Life script, keeps track of player life 
+ * and whether or not the player has died 
+ * 
+ * @author johnf2 
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLife : MonoBehaviour
+{
+    public int health;
+
+    //GameObject UI reference, set in the inspector 
+    public GameObject healthUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initially sets the player health to 5
+        health = 5;
+        Debug.Log("starting health is " + health);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthUI.gameObject.GetComponent<Text>().text = "Player Health: " + health;
+    }
+}
---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * 
+ * Player Score Script
+ * Used to keep track of player score and potentially time
+ * 
+ * @author johnf2
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour
+{
+    //used to keep track of score
+    public int score;
+    public int coinsCollected;
+
+    //public variables used to keep track of time 
+    public float time = 100;
+
+    //GameObject used to reference which UI GameObject scoreUI is
+    //the reference is set in the inspector 
+    public GameObject scoreUI;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        coinsCollected = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //time = time - Time.deltaTime;
+        //Debug.Log(time);
+        scoreUI.gameObject.GetComponent<Text>().text = "Coins: " + coinsCollected;
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+}
